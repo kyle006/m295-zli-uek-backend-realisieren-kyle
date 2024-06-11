61e17cf0ee045c2535f63484baaad565aa20379b
kyle006 <117977568+kyle006@users.noreply.github.com>
2024-06-11 11:43:31 +0200

    chore: Update names.json with additional names and refactor server.js

--- Block_4/4.2/server.js
+++ Block_4/4.2/server.js
@@ -1,5 +1,53 @@
-const fs = require('fs');
 const express = require('express');
+const fs = require('fs');
+const { request } = require('http');
 const app = express();
 const port = 3000;
 
+let me = {};
+
+app.get("/now", (request, response) => {
+    const timezone = request.query.tz;
+    response 
+        .status(200)
+        .send(new Date().toLocaleString("de-Ch", { timeZone: timezone }));
+});
+
+app.post("/names", express.json(), (request, response) => {
+    const name = request.body.name;
+    const names = fs.readFileSync(__dirname + "/names.json", "utf8");
+    const parsedNames = JSON.parse(names);
+    parsedNames.names.push(name);
+    fs.writeFileSync(__dirname + "/names.json", JSON.stringify(parsedNames));
+    response.sendStatus(201);
+});
+
+app.delete("/names", (request, response) => {
+    const name = request.body.name;
+
+    const names = fs.readFileSync(__dirname + "/names.json", "utf8");
+    const parsedNames = JSON.parse(names);
+    parsedNames.names.splice(parsedNames.names.indexOf(name), 1);
+
+    fs.writeFileSync(__dirname + "/names.json", JSON.stringify(parsedNames));
+
+    response.sendStatus(204);
+});
+
+app.get("/secret2", (request, response) => {
+    const authHeader = request.headers["authorization"];
+
+    if (authHeader === "Basic aGFja2VyOjEyMzQ=") {
+        return response.sendStatus(200);
+    } else {
+        return response.sendStatus(401);
+    }
+});
+
+app.get("/chuck", (request, response) => {
+    
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
