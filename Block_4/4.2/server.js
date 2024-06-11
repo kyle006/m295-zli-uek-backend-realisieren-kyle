7082d3919485cbec7f551925faae9d5c8676c316
kyle006 <117977568+kyle006@users.noreply.github.com>
2024-06-11 13:25:36 +0200

    chore: Refactor server.js for improved readability and consistency

--- Block_4/4.2/server.js
+++ Block_4/4.2/server.js
@@ -1,72 +1,74 @@
-const express = require('express');
-const fs = require('fs');
-const { request } = require('http');
+const express = require("express");
+const fs = require("fs");
+const { request } = require("http");
 const app = express();
 const port = 3000;
 
 let me = {
-    name:"Kyle",
-    age: 18,
-    profession: "Apprentice",
-    hobbies: ["Violin", "Gaming", "Going Outside"],
-    location: "Zürich"
-    };
+  name: "Kyle",
+  age: 18,
+  profession: "Apprentice",
+  hobbies: ["Violin", "Gaming", "Going Outside"],
+  location: "Zürich",
+};
 
 app.get("/now", (request, response) => {
-    const timezone = request.query.tz;
-    response 
-        .status(200)
-        .send(new Date().toLocaleString("de-Ch", { timeZone: timezone }));
+  const timezone = request.query.tz;
+  response
+    .status(200)
+    .send(new Date().toLocaleString("de-Ch", { timeZone: timezone }));
 });
 
 app.post("/names", express.json(), (request, response) => {
-    const name = request.body.name;
-    const names = fs.readFileSync(__dirname + "/names.json", "utf8");
-    const parsedNames = JSON.parse(names);
-    parsedNames.names.push(name);
-    fs.writeFileSync(__dirname + "/names.json", JSON.stringify(parsedNames));
-    response.sendStatus(201);
+  const name = request.body.name;
+  const names = fs.readFileSync(__dirname + "/names.json", "utf8");
+  const parsedNames = JSON.parse(names);
+  parsedNames.names.push(name);
+  fs.writeFileSync(__dirname + "/names.json", JSON.stringify(parsedNames));
+  response.sendStatus(201);
 });
 
 app.delete("/names", (request, response) => {
-    const name = request.body.name;
+  const name = request.body.name;
 
-    const names = fs.readFileSync(__dirname + "/names.json", "utf8");
-    const parsedNames = JSON.parse(names);
-    parsedNames.names.splice(parsedNames.names.indexOf(name), 1);
+  const names = fs.readFileSync(__dirname + "/names.json", "utf8");
+  const parsedNames = JSON.parse(names);
+  parsedNames.names.splice(parsedNames.names.indexOf(name), 1);
 
-    fs.writeFileSync(__dirname + "/names.json", JSON.stringify(parsedNames));
+  fs.writeFileSync(__dirname + "/names.json", JSON.stringify(parsedNames));
 
-    response.sendStatus(204);
+  response.sendStatus(204);
 });
 
 app.get("/secret2", (request, response) => {
-    const authHeader = request.headers["authorization"];
+  const authHeader = request.headers["authorization"];
 
-    if (authHeader === "Basic aGFja2VyOjEyMzQ=") {
-        return response.sendStatus(200);
-    } else {
-        return response.sendStatus(401);
-    }
+  if (authHeader === "Basic aGFja2VyOjEyMzQ=") {
+    return response.sendStatus(200);
+  } else {
+    return response.sendStatus(401);
+  }
 });
 
 app.get("/chuck", async (request, response) => {
-    const name = request.query.name || "Micheal";
+  const name = request.query.name || "Micheal";
 
-    const randomJokeResponse = await fetch("https://api.chucknorris.io/jokes/random");
-    const randomJoke = await randomJokeResponse.json();
+  const randomJokeResponse = await fetch(
+    "https://api.chucknorris.io/jokes/random"
+  );
+  const randomJoke = await randomJokeResponse.json();
 
-    let finalJoke = randomJoke.value.replace("Chuck Norris", name);
+  let finalJoke = randomJoke.value.replace("Chuck Norris", name);
 
-    response.status(200).send(finalJoke);
+  response.status(200).send(finalJoke);
 });
 
-app.patch('/me', (request, response) => {
-    const updates = request.body;
-    me = { ...me, ...updates };
-    response.send(me);
+app.patch("/me", (request, response) => {
+  const updates = request.body;
+  me = { ...me, ...updates };
+  response.send(me);
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
