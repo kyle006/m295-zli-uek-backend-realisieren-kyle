d8ebc97613d31f3527b44265cf7d21c7df57f2d3
kyle006 <117977568+kyle006@users.noreply.github.com>
2024-06-17 20:36:53 +0200

    chore: Refactor server.js to improve readability and consistency

--- Block_5/5.2/server.js
+++ Block_5/5.2/server.js
@@ -24,31 +24,32 @@ let books = [
   },
 ];
 
-let lends = [{
-    "id": 1,
-    "customer_id": "1234567890",
-    "isbn": "1",
-    "borrowed_at": "2024-05-14T09:06:05.439Z"
+let lends = [
+  {
+    id: 1,
+    customer_id: "1234567890",
+    isbn: "1",
+    borrowed_at: "2024-05-14T09:06:05.439Z",
   },
   {
-    "id": 2,
-    "customer_id": "1234567890",
-    "isbn": "2",
-    "borrowed_at": "2024-05-14T09:06:32.555Z",
-    "returned_at": "2024-05-14T09:21:08.873Z"
+    id: 2,
+    customer_id: "1234567890",
+    isbn: "2",
+    borrowed_at: "2024-05-14T09:06:32.555Z",
+    returned_at: "2024-05-14T09:21:08.873Z",
   },
   {
-    "id": 3,
-    "customer_id": "1234567890",
-    "isbn": "3",
-    "borrowed_at": "2024-05-14T09:06:35.128Z"
+    id: 3,
+    customer_id: "1234567890",
+    isbn: "3",
+    borrowed_at: "2024-05-14T09:06:35.128Z",
   },
   {
-    "id": 4,
-    "customer_id": "1234567890",
-    "isbn": "4",
-    "borrowed_at": "2024-05-14T09:26:11.877Z"
-  }
+    id: 4,
+    customer_id: "1234567890",
+    isbn: "4",
+    borrowed_at: "2024-05-14T09:26:11.877Z",
+  },
 ];
 //---------------------
 app.get("/books", (request, response) => {
@@ -81,12 +82,15 @@ app.put("/books/:isbn", (request, response) => {
 });
 //---------------------
 app.delete("/books/:isbn", (request, response) => {
-  const bookIndex = books.findIndex((book) => book.isbn === request.params.isbn);
-  if (bookIndex === -1) return response.status(404).json({ error: "Not found" });
+  const bookIndex = books.findIndex(
+    (book) => book.isbn === request.params.isbn
+  );
+  if (bookIndex === -1)
+    return response.status(404).json({ error: "Not found" });
   books.splice(bookIndex, 1);
   response.status(204).send();
 });
-
+//----
 app.patch("/books/:isbn", (request, response) => {
   const book = books.find((book) => book.isbn === request.params.isbn);
   if (!book) return response.status(404).json({ error: "Not found" });
@@ -97,52 +101,57 @@ app.patch("/books/:isbn", (request, response) => {
   if (author) book.author = author;
   response.json(book);
 });
-
+//---
 app.get("/lends", (request, response) => {
-    response.json(lends);
-  });
-  
-  app.get("/lends/:id", (request, response) => {
-      const lend = lends.find((lends) => lends.id === Number(request.params.id));
-      if (!lend) return response.status(404).json({ error: "Not found" });
-      response.json(lend);
-  });
-  
-  app.post("/lends", (request, response) => {
-    const { customer_id, isbn } = request.body;
-    if (!customer_id || !isbn) return response.status(422).json({ error: "Unprocessable" });
-  
-    const openLends = lends.filter(lends => lends.customer_id === customer_id && !l.returned_at);
-    if (openLends.length >= 5) return response.status(400).json({ error: "Maximum lends reached" });
-  
-    const bookAlreadyLent = lends.some(lends => lends.isbn === isbn && !l.returned_at);
-    if (bookAlreadyLent) return response.status(400).json({ error: "Book already lent" });
-  
-    const lend = {
-      id: Date.now().toString(),
-      customer_id,
-      isbn,
-      borrowed_at: new Date(),
-      returned_at: null
-    };
-    lends.push(lend);
-    response.json(lend);
-  });
-  
-  app.delete("/lends/:id", (request, response) => {
-    const id = request.params.id;
-    const lendIndex = lends.findIndex((lends) => lends.id == id);
+  response.json(lends);
+});
+//---
+app.get("/lends/:id", (request, response) => {
+  const lend = lends.find((lends) => lends.id === Number(request.params.id));
+  if (!lend) return response.status(404).json({ error: "Not found" });
+  response.json(lend);
+});
+//---
+app.post("/lends", (request, response) => {
+  const { customer_id, isbn } = request.body;
+  if (!customer_id || !isbn)
+    return response.status(422).json({ error: "Unprocessable" });
 
-    if (lendIndex === -1) {
-      return response.status(404).json({ error: "Not found" });
-    }
+  const openLends = lends.filter(
+    (lends) => lends.customer_id === customer_id && !l.returned_at
+  );
+  if (openLends.length >= 5)
+    return response.status(400).json({ error: "Maximum lends reached" });
 
-    lends.splice(lendIndex, 1);
-    response.status(204).send();
-  });
+  const bookAlreadyLent = lends.some(
+    (lends) => lends.isbn === isbn && !l.returned_at
+  );
+  if (bookAlreadyLent)
+    return response.status(400).json({ error: "Book already lent" });
 
-  
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+  const lend = {
+    id: Date.now().toString(),
+    customer_id,
+    isbn,
+    borrowed_at: new Date(),
+    returned_at: null,
+  };
+  lends.push(lend);
+  response.json(lend);
+});
+//---
+app.delete("/lends/:id", (request, response) => {
+  const id = request.params.id;
+  const lendIndex = lends.findIndex((lends) => lends.id == id);
 
+  if (lendIndex === -1) {
+    return response.status(404).json({ error: "Not found" });
+  }
+
+  lends.splice(lendIndex, 1);
+  response.status(204).send();
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
